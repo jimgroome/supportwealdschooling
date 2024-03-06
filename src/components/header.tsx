fea0967dd8145bed8068df6ac96f114dc23e5646
Jim Groome <james@jgroome.com>
2024-03-06 10:17:10 +0000

    Adding a bunch of packages

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import { Grid, Typography } from "@mui/material";
+
+const Header = () => (
+  <Grid container mb={4} pt={2}>
+    <Grid item xs={12}>
+      <Typography variant="h3" component="h1">
+        Support Weald Schooling
+      </Typography>
+    </Grid>
+  </Grid>
+);
+
+export default Header;
