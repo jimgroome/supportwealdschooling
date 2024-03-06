abf0f4ddd54ae4a17127d3ece1f26782f08a30c1
Jim Groome <james@jgroome.com>
2024-03-06 10:48:31 +0000

    Holding page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Header from "@/components/header";
+import { Container, CssBaseline } from "@mui/material";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
+        <CssBaseline />
+        <Container>
+          <Header />
+          <Main />
+        </Container>
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
