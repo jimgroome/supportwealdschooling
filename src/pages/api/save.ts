14c0c8e1bb0987ad691b18e5cb0ba1da9877ff26
Jim Groome <jim.groome@tghp.co.uk>
2024-07-09 21:22:15 +0100

    Mailchimp support

--- src/pages/api/save.ts
+++ src/pages/api/save.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import AWS from "aws-sdk";
+import mailchimp from "@mailchimp/mailchimp_marketing";
 
 AWS.config.update({
   region: "eu-west-2",
@@ -7,12 +8,32 @@ AWS.config.update({
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
 });
 
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: "us14",
+});
+
+async function callPing() {
+  const response = await mailchimp.ping.get();
+  console.log(response);
+}
+
 const save = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
     const { name, email, postcode, optIn } = req.body;
     const DynamoDB = new AWS.DynamoDB.DocumentClient();
     const now = new Date();
     const time = now.toISOString();
+
+    if (optIn) {
+      await mailchimp.lists.addListMember("bcbcde27c6", {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: name,
+        },
+      });
+    }
 
     await DynamoDB.put(
       {
